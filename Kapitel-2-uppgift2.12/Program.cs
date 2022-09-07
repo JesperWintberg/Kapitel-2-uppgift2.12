18f672475a00a1738f8b8e3ddbce1bd44c9e57fe
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-09-07 17:27:41 +0200

    Add project files.

---
+++ Kapitel-2-uppgift2.12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift_2_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar ska du ha bilen?");
+            string dagar = Console.ReadLine();
+            Console.WriteLine("Hur mågna kilometer ska du åka?");
+            string distans = Console.ReadLine();
+
+            float dagar_tal = float.Parse(dagar);   
+            float distans_tal = float.Parse(distans);   
+
+            float resa = 300 + distans_tal + (dagar_tal * 500) - 500; 
+
+            Console.WriteLine("Bilen kommer kosta dig " + resa); 
+        }
+    }
+}
